d788a44f616796c8524fbe9356a3fd177a7a0359
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-05-02 19:41:03 +0900

    Update 1.20.6

---
+++ src/client/java/dev/magyul/MTWModMixin.java
@@ -0,0 +1,12 @@
+package dev.magyul;
+
+import com.llamalad7.mixinextras.transformer.MixinTransformer;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class MTWModMixin implements MixinTransformer {
+    @Override
+    public void transform(IMixinInfo mixinInfo, ClassNode mixinNode) {
+
+    }
+}
