d361a4989033d0796c34343120eedd9d21caee46
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-05-18 20:25:43 +0900

    Domain Changed

--- src/client/java/dev/magyul/util/ClientUtil.java
+++ src/client/java/dev/magyul/util/ClientUtil.java
@@ -19,7 +19,7 @@ public class ClientUtil {
     };
     private static final UUID test = UUID.fromString("825203c1-484b-4935-bea2-3f7aa11e142a");
     private static final boolean local = false;
-    public static final ServerInfo mtw_info = new ServerInfo("MTW Server", local ? "localhost" : "mathwor.com", ServerInfo.ServerType.OTHER) {
+    public static final ServerInfo mtw_info = new ServerInfo("MTW Server", local ? "localhost" : "play.mathwor.com", ServerInfo.ServerType.OTHER) {
         {
             setResourcePackPolicy(ResourcePackPolicy.ENABLED);
         }
