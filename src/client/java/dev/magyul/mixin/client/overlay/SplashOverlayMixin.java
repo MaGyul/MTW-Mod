98985f144313c87f387f78b1a5cb0cae6bb4e568
magyul <magyul906@gmail.com>
2024-12-31 22:15:01 +0900

    Update background texture and adjust resource loading UI
    Replaced the splash screen background texture with `reload_background.png` to align with the new resource reloading UI. Added new demo and reload background texture files, updated associated cache and language files, and made minor adjustments to rendering logic for consistency and accuracy.

--- src/client/java/dev/magyul/mixin/client/overlay/SplashOverlayMixin.java
+++ src/client/java/dev/magyul/mixin/client/overlay/SplashOverlayMixin.java
@@ -33,7 +33,7 @@ import static net.minecraft.util.math.ColorHelper.Argb;
 @Mixin(SplashOverlay.class)
 public abstract class SplashOverlayMixin extends Overlay {
     @Unique
-    private static final Identifier BACKGROUND = Identifier.of(MTWMod.ID, "textures/gui/sprites/background.png");
+    private static final Identifier BACKGROUND = Identifier.of(MTWMod.ID, "textures/gui/reload_background.png");
 
     @Shadow @Final private MinecraftClient client;
 
@@ -81,7 +81,8 @@ public abstract class SplashOverlayMixin extends Overlay {
         RenderSystem.depthMask(false);
         RenderSystem.enableDepthTest();
         context.setShaderColor(1.0F, 1.0F, 1.0F, fadeAlpha);
-        context.drawTexture(BACKGROUND, x, y, 0, 0, width, height, 32, 32);
+        context.drawTexture(BACKGROUND, x, y, 0, 0, width, height, width, height);
+//        context.drawNineSlicedTexture(BACKGROUND, x, y, width, height, 8, 16, 16, 0, 0);
         context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         context.drawText(client.textRenderer, Text.translatable("reloading.resource"), x + 10, y + 10, withAlpha(4210752, fadeAlpha), false);
         int barX = (x + (width / 2));
